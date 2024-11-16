f497bc1ddabef6a83487ec938f0315adc6d4f2e0
batukaraman <batuhankaraman0@hotmail.com>
2024-11-17 01:39:56 +0300

    style update

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,7 +6,15 @@ function Hero() {
       <div className="hero__text">
         <span className="hero__greeting">Merhaba, ben</span>
         <h1 className="hero__name">Batuhan Karaman</h1>
-        <strong className="hero__title">Frontend Developer &amp; Tutor</strong>
+        <strong className="hero__title">
+          <div className="typewriter">
+            <span>Frontend Developer</span>
+            <span>Tutor</span>
+            <span>UI/UX Designer</span>
+            <span>Content Creator</span>
+            <span>Web Developer</span>
+          </div>
+        </strong>
       </div>
       <div className="hero__image">
         <div className="blob"></div>
